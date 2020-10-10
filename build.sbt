6ccf66a3e14fb1f740c560cac0168dd669c62716
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-10-10 15:20:55 +0200

    Init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "sbt-docker-test"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/de/maxbundscherer/sbt/docker/test/Main.scala
@@ -0,0 +1,6 @@
+package de.maxbundscherer.sbt.docker.test
+
+object Main extends App {
+
+  println("Hello Docker World!")
+}
